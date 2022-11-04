770efe9e525263a1387637cb09eaa9307267697f
Aboozar Ghaffari <aboozar.ghf@gmail.com>
2022-11-04 23:01:06 +0330

    added model proxy support to the category container

---
+++ app/Containers/MarketPalace/Category/Models/TaxonProxy.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Contains the TaxonProxy class.
+ *
+ * @copyright   Copyright (c) 2018 Aboozar Ghaffari
+ * @package     MarketPalace
+ * @author      Aboozar Ghaffari
+ * @license     MIT
+ *  */
+
+namespace App\Containers\MarketPalace\Category\Models;
+
+use App\Ship\Proxies\ModelProxy;
+
+class TaxonProxy extends ModelProxy
+{
+}
