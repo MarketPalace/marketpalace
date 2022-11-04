91c30ace71be3701ddbb5ebc2f12d5aeae634eb3
Aboozar Ghaffari <aboozar.ghf@gmail.com>
2022-11-04 23:11:43 +0330

    removed boot method

--- app/Ship/Parents/Providers/MainServiceProvider.php
+++ app/Ship/Parents/Providers/MainServiceProvider.php
@@ -26,14 +26,6 @@ abstract class MainServiceProvider extends AbstractMainServiceProvider
     }
 
     /**
-     * Perform post-registration booting of services.
-     */
-    public function boot(): void
-    {
-        parent::boot();
-    }
-
-    /**
      * Register anything in the container.
      */
     public function register(): void
