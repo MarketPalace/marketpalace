0229643f37a1e892766b3d575fd9a3a85b7f41f4
Aboozar Ghaffari <aboozar.ghf@gmail.com>
2022-11-12 22:30:43 +0330

    added create customer test

---
+++ app/Containers/MarketPalace/Customer/Tests/Unit/CreateCustomerTest.php
@@ -0,0 +1,166 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Contains the CreateCustomerTest class.
+ *
+ * @copyright   Copyright (c) 2017 Aboozar Ghaffari
+ * @author      Aboozar Ghaffari
+ * @license     MIT
+ *
+ */
+
+namespace App\Containers\MarketPalace\Customer\Tests\Unit;
+
+use App\Containers\MarketPalace\Customer\Tests\TestCase;
+use Illuminate\Support\Facades\Event;
+use App\Containers\MarketPalace\Customer\Events\CustomerWasCreated;
+use App\Containers\MarketPalace\Customer\Models\Customer;
+use App\Containers\MarketPalace\Customer\Models\CustomerProxy;
+use App\Containers\MarketPalace\Customer\Enums\CustomerType;
+
+class CreateCustomerTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function customer_can_be_created_with_minimal_data()
+    {
+        $customer = CustomerProxy::create([])->fresh();
+
+        $this->assertTrue($customer->is_active);
+        $this->assertEquals(CustomerType::__DEFAULT, $customer->type->value());
+    }
+
+    /**
+     * @test
+     */
+    public function customer_type_can_be_set_from_string()
+    {
+        $customer = new Customer();
+
+        $customer->type = CustomerType::INDIVIDUAL;
+        $customer->save();
+
+        $customer = $customer->fresh();
+        $this->assertEquals(CustomerType::INDIVIDUAL, $customer->type->value());
+    }
+
+    /**
+     * @test
+     */
+    public function customer_type_can_be_set_from_enum()
+    {
+        $customer = new Customer();
+
+        $customer->type = CustomerType::INDIVIDUAL();
+        $customer->save();
+
+        $customer = $customer->fresh();
+        $this->assertTrue(CustomerType::INDIVIDUAL()->equals($customer->type));
+    }
+
+    /**
+     * @test
+     */
+    public function individual_customer_can_be_created()
+    {
+        $john = CustomerProxy::create([
+            'type'      => CustomerType::INDIVIDUAL,
+            'firstname' => 'John',
+            'lastname'  => 'Doe'
+        ]);
+
+        $this->assertTrue($john->type->isIndividual());
+        $this->assertEquals('John', $john->firstname);
+        $this->assertEquals('Doe', $john->lastname);
+
+        $john = $john->fresh(); // still there?
+
+        $this->assertEquals('John', $john->firstname);
+        $this->assertEquals('Doe', $john->lastname);
+
+        $john = CustomerProxy::find($john->id); // I'm bastard to see if it's still there
+
+        $this->assertEquals('John', $john->firstname);
+        $this->assertEquals('Doe', $john->lastname);
+    }
+
+    /**
+     * @test
+     */
+    public function org_customer_can_be_created()
+    {
+        $acme = CustomerProxy::create([
+            'type'         => CustomerType::ORGANIZATION,
+            'company_name' => 'Acme Inc.',
+            'tax_nr'       => '19995521'
+        ]);
+
+        $this->assertEquals('Acme Inc.', $acme->company_name);
+        $this->assertEquals('19995521', $acme->tax_nr);
+    }
+
+    /** @test */
+    public function customer_was_created_event_is_fired_on_create()
+    {
+        Event::fake();
+
+        CustomerProxy::create([
+            'type' => CustomerType::ORGANIZATION
+        ]);
+
+        Event::assertDispatched(CustomerWasCreated::class);
+    }
+
+    /**
+     * @test
+     */
+    public function customer_was_created_event_contains_the_customer()
+    {
+        Event::fake();
+
+        $acme = CustomerProxy::create([
+            'type'         => CustomerType::ORGANIZATION,
+            'company_name' => 'Acme Inc.',
+            'tax_nr'       => '19995521'
+        ]);
+
+        Event::assertDispatched(CustomerWasCreated::class, function ($event) use ($acme) {
+            return $event->getCustomer()->id   == $acme->id
+                && $event->getCustomer()->name == $acme->name;
+        });
+    }
+
+    /** @test */
+    public function timezone_can_be_set()
+    {
+        $bamakan = Customer::create([
+            'timezone' => 'Africa/Bamako',
+        ])->fresh();
+
+        $this->assertEquals('Africa/Bamako', $bamakan->timezone);
+    }
+
+    /** @test */
+    public function currency_can_be_set()
+    {
+        $japanese = Customer::create([
+            'currency' => 'JPY',
+        ])->fresh();
+
+        $this->assertEquals('JPY', $japanese->currency);
+    }
+
+    /** @test */
+    public function lifetime_value_can_be_set()
+    {
+        $japanese = Customer::create([
+            'ltv' => 2700.35,
+        ])->fresh();
+
+        $this->assertEquals(2700.35, $japanese->ltv);
+        $this->assertIsFloat($japanese->ltv);
+    }
+}
